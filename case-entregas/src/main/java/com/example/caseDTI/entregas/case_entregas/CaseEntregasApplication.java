e04b1fe69cf36b876590f5d428ca1718f84622ab
nathaliacandioto <nathaliacandioto@gmail.com>
2025-08-29 12:05:16 -0300

    criacao do projeto

---
+++ case-entregas/src/main/java/com/example/caseDTI/entregas/case_entregas/CaseEntregasApplication.java
@@ -0,0 +1,13 @@
+package com.example.caseDTI.entregas.case_entregas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaseEntregasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaseEntregasApplication.class, args);
+	}
+
+}
---
+++ case-entregas/src/test/java/com/example/caseDTI/entregas/case_entregas/CaseEntregasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.caseDTI.entregas.case_entregas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaseEntregasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
